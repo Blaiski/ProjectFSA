0c0f974144a645c5b15b7fe1ba045b73d604f717
Mujtaba <mujtaba.alshakhouri@gmail.com>
2025-01-15 21:07:07 +1300

    added login functionality

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var session = require('express-session');
 var path = require('path');
-var conn =require('./dbConfig');
+var connection =require('./dbConfig');
  
 var app = express();
 app.use(session({
@@ -17,14 +17,23 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
  
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 //==================================
 
 app.get('/login', function(req, res, next) {
-	res.render('login.ejs', { title: 'Home' });
+	res.render('login.ejs', { title: 'Login' });
+});
+
+app.get('/membersOnly', function(req, res, next) {
+	if(req.session.loggedin){
+		res.render('membersOnly');
+	}
+	else{
+		res.send('Please login to view this page!');
+	}
 });
 
 app.get('/', function(req, res, next) {
@@ -39,6 +48,41 @@ app.get('/auck', function(req, res, next) {
 app.get('/beaches', function(req, res, next) {
 	res.render('beaches', { title: 'Beaches Page' });
 });
+
+//app.get('/auth', function(reg,res){});
+
+app.post('/auth', function(req,res){
+	let name = req.body.username;
+	let password = req.body.password;
+	
+	if(name && password){
+		connection.query(
+			'SELECT * FROM users WHERE name= ? AND password=?', 
+			[name, password],
+			function(error, results, fields){
+				if(error) throw error;//this line will force the application to terminate
+				if (results.length > 0){
+					req.session.loggedin = true;
+					req.session.username = name;
+					res.redirect('/membersOnly');
+				}else{
+					res.send('Incorrect Username and/or Password!');
+				}
+				res.end();
+			}
+		);
+	}
+	///execution will jump to this line if either name or password was null/nondefined
+	else{
+		res.send("Please enter Username and Password!");
+		res.end();
+	}
+});
+
+app.get('/logout', (req,res)=>{
+	req.session.destroy();
+	res.redirect('/');
+})
 
 app.listen(3000);
 console.log('Node app is running on port 3000');
